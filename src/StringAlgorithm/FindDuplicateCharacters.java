cd7cd600699b88ae513f733c54fb0e312de5ae12
yu yuan <578402453@qq.com>
2015-03-09 11:21:49 +0800

    finish the algorithm

--- src/StringAlgorithm/FindDuplicateCharacters.java
+++ src/StringAlgorithm/FindDuplicateCharacters.java
@@ -2,12 +2,13 @@ package StringAlgorithm;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class FindDuplicateCharacters {
 
     public static void printDuplicateCharacters(String s){
         if(StringUtil.isEmpty(s)){
-            System.out.println("the string cannot be null or empty");
+            System.out.println("Error : the string cannot be null or empty");
             return;
         }
         char[] chars = s.toCharArray();
@@ -19,5 +20,21 @@ public class FindDuplicateCharacters {
                 map.put(c,1);
             }
         }
+
+        int flag = 0;
+        Set<Map.Entry<Character,Integer>> entrySet = map.entrySet();
+        for(Map.Entry<Character,Integer> entry : entrySet){
+            if(entry.getValue()>1){
+                flag++;
+                System.out.printf("%s : %d %n", entry.getKey(), entry.getValue());
+            }
+        }
+        if(flag == 0){
+            System.out.println("There're no duplicate characters in the string");
+        }
+    }
+
+    public static void main(String[] args) {
+        printDuplicateCharacters("");
     }
 }
